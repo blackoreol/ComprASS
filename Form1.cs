df7fbf0d5b5b9ffb8070cdcbc1f0d01279193fe6
Dmytro Konzersky <dimakonzer@gmail.com>
2023-10-19 14:50:33 +0300

    19.10.2023

--- Form1.cs
+++ Form1.cs
@@ -47,68 +47,57 @@ namespace ComprASS
                 return;
             }
 
-            string ffmpegCommand = "ffmpeg";
-
             // Ускорение видео в 4 раза
             string accelerateArguments = $"-i \"{inputFilePath}\" -vf \"setpts=0.2*PTS\" -an \"{outputFilePath}\" -y";
-
-            ProcessStartInfo accelerateStartInfo = new ProcessStartInfo
-            {
-                FileName = ffmpegCommand,
-                Arguments = accelerateArguments,
-                RedirectStandardOutput = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-
-            using (Process accelerateProcess = new Process { StartInfo = accelerateStartInfo })
-            {
-                accelerateProcess.Start();
-                accelerateProcess.WaitForExit();
-            }
+            RunFFmpegCommand(accelerateArguments);
 
             // Получение длительности ускоренного видео
-            string ffprobeCommand = "ffprobe";
             string ffprobeArguments = $"-v error -select_streams v:0 -show_entries stream=duration -of default=noprint_wrappers=1:nokey=1 \"{outputFilePath}\"";
+            double duration = GetVideoDuration(ffprobeArguments);
 
-            ProcessStartInfo ffprobeStartInfo = new ProcessStartInfo
-            {
-                FileName = ffprobeCommand,
-                Arguments = ffprobeArguments,
-                RedirectStandardOutput = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
+            // Обрезка последних 3/4 видео
+            string trimArguments = $"-i \"{outputFilePath}\" -t {duration * 0.25} -c:v copy -c:a copy \"{outputFilePath}\" -y";
+            RunFFmpegCommand(trimArguments);
 
-            using (Process ffprobeProcess = new Process { StartInfo = ffprobeStartInfo })
+            MessageBox.Show("Видео успешно ускорено и обрезано.");
+        }
+
+        private void RunFFmpegCommand(string arguments)
+        {
+            using (Process process = new Process())
             {
-                ffprobeProcess.Start();
-                string outputDuration = ffprobeProcess.StandardOutput.ReadToEnd();
-                ffprobeProcess.WaitForExit();
+                process.StartInfo.FileName = "ffmpeg";
+                process.StartInfo.Arguments = arguments;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardOutput = true;
+
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+
+        private double GetVideoDuration(string arguments)
+        {
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = "ffprobe";
+                process.StartInfo.Arguments = arguments;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardOutput = true;
+
+                process.Start();
+                string outputDuration = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
 
                 if (double.TryParse(outputDuration, out double duration))
                 {
-                    // Обрезка последних 3/4 видео
-                    string trimArguments = $"-i \"{outputFilePath}\" -t {duration * 0.2} -c:v copy -c:a copy \"{outputFilePath}\" -y";
-
-                    ProcessStartInfo trimStartInfo = new ProcessStartInfo
-                    {
-                        FileName = ffmpegCommand,
-                        Arguments = trimArguments,
-                        RedirectStandardOutput = true,
-                        UseShellExecute = false,
-                        CreateNoWindow = true
-                    };
-
-                    using (Process trimProcess = new Process { StartInfo = trimStartInfo })
-                    {
-                        trimProcess.Start();
-                        trimProcess.WaitForExit();
-                    }
+                    return duration;
                 }
-            }
 
-            MessageBox.Show("Видео успешно ускорено и обрезано.");
+                return 0.0;
+            }
         }
     }
 }
